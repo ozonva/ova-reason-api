0731dc9312642bf55eac9f7bbb9e1d65ce1a248b
Лаптев Михаил Александрович <malapte4@mts.ru>
2021-08-10 13:17:31 +0300

    add InvertMap and FilterOdds

--- cmd/ova-reason-api/main.go
+++ cmd/ova-reason-api/main.go
@@ -19,6 +19,21 @@ func main() {
 			fmt.Printf("result[%d][%d] = %d\n", i, j, result[i][j])
 		}
 	}
+	fmt.Println("___________________________")
 
-	fmt.Println("Hello, it's ova-reason-api!")
+	sourceMap := map[int]string{1: "one", 2: "two", 3: "three"}
+	result2 := utils.InvertMap(sourceMap)
+	for key, value := range result2 {
+		fmt.Printf("invertedMap[%s] = %d\n", key, value)
+	}
+
+	fmt.Println("___________________________")
+	sliceForFilter := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	result3 := utils.FilterOdds(sliceForFilter)
+
+	fmt.Printf("Even numbers: ")
+	for _, value := range result3 {
+		fmt.Printf("%d,", value)
+	}
+	fmt.Println("\nHello, it's ova-reason-api!")
 }
--- internal/utils/sliceDivider.go
+++ internal/utils/sliceDivider.go
@@ -14,3 +14,36 @@ func DivideToBatches(slice []int, batchSize int) [][]int {
 
 	return result
 }
+
+func InvertMap(sourceMap map[int]string) map[string]int {
+	result := make(map[string]int, len(sourceMap))
+	for key, value := range sourceMap {
+		result[value] = key
+	}
+
+	return result
+}
+
+var oddNumbers = []int{1, 3, 5, 7, 9, 11, 13, 15}
+
+func FilterOdds(slice []int) []int {
+	var result []int
+	for _, value := range slice {
+
+		if !findElement(oddNumbers, value) {
+			result = append(result, value)
+		}
+	}
+
+	return result
+}
+
+func findElement(slice []int, element int) bool {
+	for _, elem := range slice {
+		if element == elem {
+			return true
+		}
+
+	}
+	return false
+}
