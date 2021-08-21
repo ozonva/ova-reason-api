08d83e4fa9059c4c9781793ca771ab5c65e63689
Лаптев Михаил Александрович <malapte4@mts.ru>
2021-08-22 00:38:26 +0300

    tests are ready but don't work

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/ozonva/ova-reason-api
 
 go 1.16
 
-require github.com/golang/mock v1.6.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.16.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ internal/flusher/flusher_test.go
@@ -0,0 +1,68 @@
+package flusher_test
+
+import (
+	"errors"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/ozonva/ova-reason-api/internal/flusher"
+	"github.com/ozonva/ova-reason-api/internal/mocks"
+	"github.com/ozonva/ova-reason-api/internal/model"
+)
+
+var _ = Describe("Flusher", func() {
+
+	var (
+		ctrl       *gomock.Controller
+		mockRepo   *mocks.MockRepo
+		reasons    []model.Reason
+		flusherObj flusher.Flusher
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockRepo = mocks.NewMockRepo(ctrl)
+
+		flusherObj = flusher.NewFlusher(2, mockRepo)
+		reasons = []model.Reason{
+			*model.New(1, 1, 1, "forgot keys"),
+			*model.New(5, 5, 5, "lost my wallet"),
+			*model.New(6, 6, 6, "want bonus"),
+			*model.New(7, 7, 7, "my friend asked"),
+			*model.New(8, 8, 8, "want to eat"),
+		}
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	Describe("Flushing slice of reasons into base", func() {
+		Context("With no error", func() {
+			gomock.InOrder(
+				mockRepo.EXPECT().AddEntities(gomock.Len(2)).Return(nil),
+				//mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
+				//mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
+			)
+			It("should return null", func() {
+				result := flusherObj.Flush(reasons)
+				Expect(result).To(BeNil())
+			})
+		})
+
+		Context("With errors", func() {
+			gomock.InOrder(
+				mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
+				mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
+				mockRepo.EXPECT().AddEntities(gomock.Any()).Return(errors.New("no enough space")),
+			)
+			It("should return null", func() {
+				result := flusherObj.Flush(reasons)
+				Expect(result).ToNot(BeNil())
+				Expect(len(result)).To(Equal(1))
+				Expect(result[0]).To(Equal(reasons[5]))
+
+			})
+		})
+	})
+})
