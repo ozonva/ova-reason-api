85cce1cb7d57b6ebc69798aa3e81e11a3b7c87b3
Лаптев Михаил Александрович <malapte4@mts.ru>
2021-08-22 15:48:15 +0300

    add saver

--- cmd/ova-reason-api/main.go
+++ cmd/ova-reason-api/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/gomega"
 	"github.com/ozonva/ova-reason-api/internal/flusher"
 	"github.com/ozonva/ova-reason-api/internal/mocks"
 	"github.com/ozonva/ova-reason-api/internal/model"
+	"github.com/ozonva/ova-reason-api/internal/saver"
+	"strconv"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 )
@@ -15,43 +16,37 @@ import (
 func main() {
 
 	var (
-		ctrl       *gomock.Controller
-		mockRepo   *mocks.MockRepo
-		reasons    []model.Reason
+		ctrl     *gomock.Controller
+		mockRepo *mocks.MockRepo
+
 		flusherObj flusher.Flusher
+		saverObj   saver.Saver
 	)
 	RegisterFailHandler(Fail)
 	ctrl = gomock.NewController(GinkgoT())
 	mockRepo = mocks.NewMockRepo(ctrl)
 
 	flusherObj = flusher.NewFlusher(2, mockRepo)
-	reasons = []model.Reason{
-		*model.New(1, 1, 1, "forgot keys"),
-		*model.New(5, 5, 5, "lost my wallet"),
-		*model.New(6, 6, 6, "want bonus"),
-		*model.New(7, 7, 7, "my friend asked"),
-		*model.New(8, 8, 8, "want to eat"),
+
+	mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil).Times(100000) //.Do(func() { fmt.})
+
+	saverObj = saver.NewSaver(5, flusherObj)
+
+	go runGenerator("goroutine1", 1000, saverObj)
+	go runGenerator("goroutine2", 1500, saverObj)
+	go runGenerator("goroutine3", 1800, saverObj)
+	time.Sleep(20 * time.Second)
+
+	saverObj.Close()
+
+}
+
+func runGenerator(id string, timeout int, saverObj saver.Saver) {
+	cnt := 0
+	for {
+		time.Sleep(time.Millisecond * time.Duration(timeout))
+		cnt++
+		newReason := *model.New(1, 1, 1, id+" "+strconv.Itoa(cnt))
+		saverObj.Save(newReason)
 	}
-
-	mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil).Times(3)
-
-	result := flusherObj.Flush(reasons)
-	Expect(result).Should(BeNil())
-
-	fmt.Println(result)
-
-	gomock.InOrder(
-		mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
-		mockRepo.EXPECT().AddEntities(gomock.Any()).Return(nil),
-		mockRepo.EXPECT().AddEntities(gomock.Any()).Return(errors.New("no enough space")),
-	)
-
-	result = flusherObj.Flush(reasons)
-
-	fmt.Println(len(result))
-	fmt.Println(result[0].String())
-	Expect(result).ToNot(BeNil())
-	Expect(len(result)).To(Equal(1))
-	Expect(result[0]).To(Equal(reasons[4]))
-
 }
--- internal/saver/saver.go
+++ internal/saver/saver.go
@@ -1,8 +1,12 @@
 package saver
 
 import (
+	"fmt"
 	"github.com/ozonva/ova-reason-api/internal/flusher"
 	"github.com/ozonva/ova-reason-api/internal/model"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Saver interface {
@@ -16,21 +20,60 @@ func NewSaver(
 	capacity uint,
 	flusher flusher.Flusher,
 ) Saver {
-	return &saver{
-		capacity: capacity,
-		flusher:  flusher,
+	newSaver := saver{
+		capacity:    capacity,
+		flusherObj:  flusher,
+		tempStorage: make([]model.Reason, 0, capacity),
 	}
+	go newSaver.flushByTimeout()
+	return &newSaver
 }
 
 type saver struct {
-	capacity uint
-	flusher  flusher.Flusher
+	capacity    uint
+	flusherObj  flusher.Flusher
+	tempStorage []model.Reason
+	mu          sync.Mutex
+	totalCnt    int
 }
 
-func (s saver) Save(entity model.Reason) {
-	panic("implement me")
+func (s *saver) Save(entity model.Reason) {
+	s.mu.Lock()
+	s.tempStorage = append(s.tempStorage, entity)
+	s.mu.Unlock()
 }
 
-func (s saver) Close() {
-	panic("implement me")
+func (s *saver) Close() {
+
+	s.flush()
+
+	fmt.Println("TotalCnt= " + strconv.Itoa(s.totalCnt))
+}
+
+func (s *saver) flush() {
+
+	s.mu.Lock()
+	whatToFlush := s.tempStorage
+	time.Sleep(time.Millisecond * 500)
+	s.tempStorage = make([]model.Reason, 0, s.capacity)
+	s.mu.Unlock()
+
+	fmt.Println("Saver: Start flushing " + strconv.Itoa(len(whatToFlush)) + " reasons")
+	for ind, reason := range whatToFlush {
+		fmt.Println("Saver: " + strconv.Itoa(ind) + " " + reason.Why)
+	}
+	s.totalCnt += len(whatToFlush)
+
+	if len(whatToFlush) > 0 {
+		s.flusherObj.Flush(whatToFlush)
+	}
+}
+
+func (s *saver) flushByTimeout() {
+
+	for {
+		time.Sleep(5 * time.Second)
+		go s.flush()
+	}
+
 }
